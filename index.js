77d5cfa287f37c6a005774d7e9cbcb1712400b54
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-13 13:04:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 5;
+let multiply = num1*num2;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max = Math.max(3,12,14,20)
+
+console.log(multiply)
+
